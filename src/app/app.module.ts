5bb33a11fd232a27f8ab04a84185e70f144b0614
Enock <enocksm@outlook.com>
2024-01-08 13:40:17 -0300

    vasco da gama no projeto front-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BigCardclsComponent } from './components/big-cardcls/big-cardcls.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     MenuTitleComponent,
-    BigCardclsComponent
+    BigCardclsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
