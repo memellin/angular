10189ce10489eba7b0ab1f38831fa3766d41a9b6
Enock <enocksm@outlook.com>
2024-01-11 13:16:56 -0300

    adicionando dinamicidade aos componentes, escalabilidade

--- src/app/components/big-cardcls/big-cardcls.component.ts
+++ src/app/components/big-cardcls/big-cardcls.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-cardcls',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-cardcls.component.css']
 })
 export class BigCardclsComponent implements OnInit {
+
+  @Input()
+  cardPhoto:string =""
+  @Input()
+  cardTitle:string =""
+  @Input()
+  cardDescription:string =""
 
   constructor() { }
 
